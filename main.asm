83e2f774241950b9fe9d68b1d7e5d2ae1fda9c6e
gustavodezan <63751057+gustavodezan@users.noreply.github.com>
2021-05-08 13:53:44 -0300

    restart
    Quando o input for R (definido nas macros) ele retorna para LOAD_GAME_START, assim reiniciando o jogo

--- main.asm
+++ main.asm
@@ -4,16 +4,26 @@
 # -----------------------------------------
 .include "./assets/assets_data/title.data"
 .include "./assets/assets_data/Adventures_of_Lolo_Title.data"
-.include "./assets/assets_data/vermelho.data"
-.include "./assets/assets_data/lolinho.data"
+.include "./assets/assets_data/lolo_walk.data"
 .include "./scripts/macros.asm"
 .include "./scripts/andar.asm"
+.include "./scripts/mapa.asm"
 .eqv MMIO_add 0xff200004 # Data (ASCII value)
 .eqv MMIO_set 0xff200000 # Control (boolean)
 
-# -----------
-# Constantes
-# -----------
+# ----------------------
+# Player State Machine
+# ----------------------
+# 0 - Player State Free
+# 1 - Player State WALK
+# 3 - DEATH?
+STATE:	.byte 0, 0		# Seria útil diferenciar state free de state walk?
+POS_X:  .half 0               # x e y
+POS_Y:  .half 0
+# O vetor DIR guarda a última direção para a qual o sprite olhou
+# Vai de 0 até 3, seguindo o sentido de um plano cartesiano
+DIR:    .byte 1
+
 
 # Obs.: talvez seja necessário diminuir a quantidade de includes? Se não, passar as .eqv para outra pasta
 
@@ -21,26 +31,30 @@
 # -----------------------------------------------------------------------------
 # Imprimir na tela o menu inicial
 # -----------------------------------------------------------------------------
-	li s1,80 # s1 será equivalente à X
-	li s2,0 # s2 será equivalente à Y
+
+LOAD_GAME_START:
+	li s1,160
+	li s2,120
+	update_pos(s1,s2,POS_X,POS_Y,t2)	
 	li a6,40
 	print_img(Adventures_of_Lolo_Title,a6) # title é a correta para 320 x 240, Adventures_of_Lolo_Title é para 240 x 240
 	li a6,0
-MAIN:
-
-
+	
 # Receber o input do teclado
 # Após isso movimentar o personagem
 # A movimentação ainda não está de acordo com o tipo de input -> implementar em andar.asm
-INPUT:
+GAME_LOOP:
+	load_pos(POS_X,POS_Y,s1,s2)
+	
 	li t0, MMIO_set # ready bit MMIO
 	lb t1,(t0)
-	beqz t1, INPUT # wait time enquanto ready bit == 0
+	beqz t1, GAME_LOOP # wait time enquanto ready bit == 0
 	li a0, MMIO_add # Data address MMIO
 	lw a0,(a0) # Recupera o valor de MMIO
-	andar(a0,lolinho,Adventures_of_Lolo_Title,s1,s2)
-
-	j INPUT
+	#check_state(a0,STATE,t2)
+	andar(a0,lolo_walk,Adventures_of_Lolo_Title,s1,s2)
+	
+	j GAME_LOOP
 	Exit()
 # Pensamentos sobre a impressão de imagens:
 # para printar uma imagem na tela com o script (em andar.asm) todos os registradores temp são usados,
@@ -52,10 +66,9 @@ INPUT:
 # s1 -> X
 # s2 -> Y
 # s9 -> dimensão máxima da tela (é definido sempre que é necessário)
+# s10 -> vetor de x por y
 # Com o uso de s1 e s2 para determinar o a posição na tela sempre que algo for andar, fica mais fácil de implementar
 # o desenho de partes menores do mapa.
-
-
 
 
 
