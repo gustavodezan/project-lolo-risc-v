d26c78fa69ccbbeb7fb200cacf482753b66d4fe5
gustavodezan <63751057+gustavodezan@users.noreply.github.com>
2021-05-20 05:38:23 -0300

    Update andar.asm

--- Projeto_Lolo - Final Ver/scripts/andar.asm
+++ Projeto_Lolo - Final Ver/scripts/andar.asm
@@ -1,6 +1,6 @@
 # Print Image
-# %sprite passa o sprite que ser· movimentado
-# %grid_reg passa o registrador que contÈm a informaÁ„o de onde a imagem deve aparecer na tela
+# %sprite passa o sprite que ser√° movimentado
+# %grid_reg passa o registrador que cont√©m a informa√ß√£o de onde a imagem deve aparecer na tela
 .eqv RIGHT 100
 .eqv UP 119
 .eqv LEFT 97
@@ -11,27 +11,27 @@
 .eqv SPACE 32
 
 .macro animate_sprite(%sprite,%grid_reg,%frame,%dir)
-	la t0,%sprite # carregar o endereÁo do sprite em t0 apenas para passar para t1 e t2
+	la t0,%sprite # carregar o endere√ßo do sprite em t0 apenas para passar para t1 e t2
 	lw t1, 0(t0) # x -> primeira word - inicia em 0xFF000000 e termina em 0xFF00013F
 	lw t2, 4(t0) # y -> segunda word
-	mul t3,t2,t2 # y * y para obter a ·rea -> t3 = ·rea (16x16)
+	mul t3,t2,t2 # y * y para obter a √°rea -> t3 = √°rea (16x16)
 	addi t0,t0,8
-	# Definir a posiÁ„o inicial da imagem na tela
+	# Definir a posi√ß√£o inicial da imagem na tela
 	li s0, 0xFF000000
 	add s0,s0,%grid_reg
 	sub s8,t1,t2 # Encontrar valor de line_break para os pixels
-	# t1 passa o valor da dimens„o x total da imagem, n„o de um ˙nico frame
+	# t1 passa o valor da dimens√£o x total da imagem, n√£o de um √∫nico frame
 	mult_div(div,t6,t1,4)
-	mv t1,t6 # transformar t1 no valor de um frame e n„o do sprite inteiro -> totalFrames
+	mv t1,t6 # transformar t1 no valor de um frame e n√£o do sprite inteiro -> totalFrames
 	la t4,%dir
 	lb t4,0(t4)
 	mul t4,t1,t4
 	add t0,t0,t4
 	image_cicle(%sprite,t1,t2,%frame,t4,t5,t6)
 	add t0,t0,t4
-	# Encontrar o n∫ de pixels a serem pulados
-	li s9,320 # dimens„o m·xima da tela
-	sub t1,s9,t2 # t2 armazena da dimens„o m·xima - o valor x do sprite
+	# Encontrar o n¬∫ de pixels a serem pulados
+	li s9,320 # dimens√£o m√°xima da tela
+	sub t1,s9,t2 # t2 armazena da dimens√£o m√°xima - o valor x do sprite
 	mv s9,s8 # usar s9 como val de line_break
 	li t6,0 # contador quebra de linha
 	li t4, 0 # contador de parada
@@ -48,20 +48,20 @@ IMPRIMEA:
 NEW_LINEA:
 	add s0,s0,t1
 	add t0,t0,s9 # valor de line_break com retorno para o mesmo ponto inicial em X na linha seguinte
-	li t6,0 # retornar t6 para 0 para recomeÁar a contagem
+	li t6,0 # retornar t6 para 0 para recome√ßar a contagem
 	j IMPRIMEA
 BREAK_ANIMATION:
 .end_macro
 
-# Printa imagens est·ticas na tela
+# Printa imagens est√°ticas na tela
 .macro print_img(%sprite,%grid_reg,%f_reg,%f_adress)
-	la t0,%sprite # carregar o endereÁo de tela em t0
+	la t0,%sprite # carregar o endere√ßo de tela em t0
 	lw t1, 0(t0) # x -> primeira word - inicia em 0xFF000000 e termina em 0xFF00013F
 	lw t2, 4(t0) # y -> segunda word
-	mul t3,t1,t2 # x * y para obter a ·rea -> t3 = ·rea
-	addi t0,t0,8 # 0 È x, 4 È y, portanto em 8 comeÁa a imagem
+	mul t3,t1,t2 # x * y para obter a √°rea -> t3 = √°rea
+	addi t0,t0,8 # 0 √© x, 4 √© y, portanto em 8 come√ßa a imagem
 	li %f_reg, %f_adress
-	li s9,320 #dimens„o m·xima da tela
+	li s9,320 #dimens√£o m√°xima da tela
 	sub t2,s9,t1
 	add %f_reg,%f_reg,%grid_reg
 	li t6,0 # contador quebra de linha
@@ -78,40 +78,40 @@ IMPRIME:
 	j IMPRIME
 NEW_LINE: 
 	add %f_reg,%f_reg,t2
-	li t6,0 # retornar t6 para 0 para recomeÁar a contagem
+	li t6,0 # retornar t6 para 0 para recome√ßar a contagem
 	j IMPRIME
 BREAK_PRINT:
 .end_macro
 
-# Player collision deve vir sempre que for chamada a funÁ„o de movimentar o jogador
-# Desse modo, ser· checado se h· colis„o no prÛximo espaÁo que o Lolo ir· ocupar
-# se houver, ele n„o movimenta, se estiver livre, ele movimenta normalmente
+# Player collision deve vir sempre que for chamada a fun√ß√£o de movimentar o jogador
+# Desse modo, ser√° checado se h√° colis√£o no pr√≥ximo espa√ßo que o Lolo ir√° ocupar
+# se houver, ele n√£o movimenta, se estiver livre, ele movimenta normalmente
 .macro check_collision(%x,%y,%x_mv,%y_mv,%col_type)
-	# Checar se est· em uma das bordas do mapa
-	# como todas as unidades est„o sujeitas ao mapa, serve para todas
+	# Checar se est√° em uma das bordas do mapa
+	# como todas as unidades est√£o sujeitas ao mapa, serve para todas
 	mv t5,%x_mv
 	mv t6,%y_mv
 	add %x_mv,%x,%x_mv
 	add %y_mv,%y,%y_mv
-	li t0,52 # pixel x onde comeÁa o mapa, apÛs a parede
-	li t1,28 # pixel y onde comeÁa o mapa, apÛs a parede
-	ble %x_mv,t0,V_END_CHECK_COL # checar colis„o na esquerda
-	ble %y_mv,t1,V_END_CHECK_COL # checar colis„o encima
+	li t0,52 # pixel x onde come√ßa o mapa, ap√≥s a parede
+	li t1,28 # pixel y onde come√ßa o mapa, ap√≥s a parede
+	ble %x_mv,t0,V_END_CHECK_COL # checar colis√£o na esquerda
+	ble %y_mv,t1,V_END_CHECK_COL # checar colis√£o encima
 	li t0,220
 	li t1,196
-	bge %x_mv,t0,V_END_CHECK_COL # checar colis„o na direita
-	bge %y_mv,t1,V_END_CHECK_COL #checar colis„o inferior
+	bge %x_mv,t0,V_END_CHECK_COL # checar colis√£o na direita
+	bge %y_mv,t1,V_END_CHECK_COL #checar colis√£o inferior
 	
 CHECK_COL_STEP2:
 	# Passo 2:
-	# checar se h· algum objeto com colis„o perto
+	# checar se h√° algum objeto com colis√£o perto
 	li a7,1
 	la s8,CURRENT_MAP
 	li t4,0
 
 # SUPERIOR_ESQUERDO: X = 56 Y = 32
 # SUPERIOR_DIREITO: X = 44 Y = 32
-# INFERIOR_ESQUERDO: X = 56 Y =®20
+# INFERIOR_ESQUERDO: X = 56 Y =¬®20
 # INFERIOS_DIREITO: X = 44 Y = 20
 	j START_POINT_CHECKING
 	
@@ -182,7 +182,7 @@ NEXT_CHECK_COL0:
 	call CHEST_ROUTINE
 NEXT_CHECK_COL1:	
 	beqz %col_type,V_END_CHECK_COL
-	mv %col_type,t1 # se a colis„o for do poder, informar que a colis„o ocorreu
+	mv %col_type,t1 # se a colis√£o for do poder, informar que a colis√£o ocorreu
 
 END_CHECK_COLLISION:
 .end_macro
@@ -194,14 +194,14 @@ CLEAR:
 	mul s10,%y,s9
 	add s10,s10,%x
 	li t1,16
-	mul t3,t1,t1 # x * y para obter a ·rea -> t3 = ·rea
+	mul t3,t1,t1 # x * y para obter a √°rea -> t3 = √°rea
 	la t0,%old_sprite # carregar em t0 o valor do sprite anterior, para acessar as cores dele
 	addi t0,t0,8
 	li s0, 0xFF000000
 	add s0,s0,s10
-	li s9,320 # dimens„o m·xima da tela
+	li s9,320 # dimens√£o m√°xima da tela
 	sub t2,s9,t1
-	# Como X e Y n„o est„o em vetor, posso facilmente achar a posiÁ„o relativa do background em 240x240
+	# Como X e Y n√£o est√£o em vetor, posso facilmente achar a posi√ß√£o relativa do background em 240x240
 	la s9,%old_sprite
 	lw s9,0(s9) # armazenar em s9 o valor y do mapa
 	mul s10,%y,s9
@@ -227,7 +227,7 @@ CLEAR_LOOP:
 CLEAR_LINE:
 	add s0,s0,t2
 	add t0,t0,s9 # valor de line_break com retorno para o mesmo ponto inicial em X na linha seguinte
-	li t6,0 # retornar t6 para 0 para recomeÁar a contagem
+	li t6,0 # retornar t6 para 0 para recome√ßar a contagem
 	j CLEAR_LOOP
 END_CLEAR:
 .end_macro
